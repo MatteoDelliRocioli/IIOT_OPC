3c4a1846290755bc98a7492a244eb8bf8c1e4909
andb70 <andb70@gmail.com>
2019-05-03 23:35:16 +0200

    Added PlantStates data access

--- IIOT_OPC.Shared/Models/DailyProduction.cs
+++ IIOT_OPC.Shared/Models/DailyProduction.cs
@@ -3,7 +3,6 @@
     using System;
     public class DailyProduction: DbObject
     {
-        public int Id { get; set; }
         public DateTime TimeStamp { get; set; }
         public int NumPieces { get; set; }
         public int NumPiecesRejected { get; set; }
---
+++ IIOT_OPC.Shared/Models/PlantState.cs
@@ -0,0 +1,9 @@
+﻿namespace IIOT_OPC.Shared.Models
+{
+    using System;
+    public class PlantStates : DbObject
+    {
+        public DateTime TimeStamp { get; set; }
+        public int PlantState { get; set; }
+    }
+}
