2f70dc09b6def44f0cce41baa83ba8a89acb6d52
delli <matteo.delli.rocioli.pe@gmail.com>
2019-05-09 11:37:32 +0200

    Added casic structure of program to handle user input as date

---
+++ IIOT_OPC.InputHandler/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace IIOT_OPC.InputHandler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //dato giornaliero sul polling
+
+            /*check date*/
+            /*calculate oee*/
+            /**/
+            /**/
+            ProgramState state = ProgramState.checkingDate;
+            DateTime dateToCheck;
+            DateTime defaultDate = new DateTime();
+
+            while (true)
+            {
+                switch (state)
+                {
+                    case ProgramState.checkingDate:
+                        dateToCheck = checkDateFromUser();
+                        if (dateToCheck != defaultDate)
+                        {
+                            state = ProgramState.oeeCalculation;
+                        }
+                        break;
+                    case ProgramState.getDateFromDb:
+                        Console.WriteLine("sono dentro date");
+                        Console.ReadLine();
+                        break;
+                    case ProgramState.oeeCalculation:
+                        Console.WriteLine("sono dentro calculation");
+                        Console.ReadLine();
+                        break;
+                    case ProgramState.writeOutPut:
+                        Console.WriteLine("sono dentro writeOutput");
+                        Console.ReadLine();
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        static DateTime checkDateFromUser()
+        {
+            DateTime userDateTime = new DateTime();
+
+            Console.WriteLine("Enter a date (ex. dd/MM/YYYY): ");
+            if (DateTime.TryParse(Console.ReadLine(), out userDateTime))
+            {
+                Console.WriteLine("The date typed is: " + userDateTime);
+                return userDateTime;
+            }
+            else
+            {
+                Console.WriteLine("You have entered an incorrect value.");
+                Console.WriteLine("Try again...");
+            }
+            Console.ReadLine();
+            Console.Clear();
+            return userDateTime;
+        }
+
+        public enum ProgramState
+        {
+            checkingDate = 0x10,
+            getDateFromDb = 0x20,
+            oeeCalculation = 0x30,
+            writeOutPut = 0x40
+        }
+    }
+}
